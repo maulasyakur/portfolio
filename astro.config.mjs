9f4e88eccae505030a71e18368a84572119b675f
maulasyakur <maulaahmad18@gmail.com>
2026-07-30 23:55:10 +0300

    add github pages config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://maulasyakur.github.io",
+  base: "/portfolio", // Must match your GitHub repository name exactly
+  output: "static", // GitHub Pages only supports static sites
+});
