82e6435f2b34e8407cd56f0c694a6f747f6c63db
maulasyakur <maulaahmad18@gmail.com>
2026-07-31 10:51:26 +0300

    fix env import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+const baseUrl = import.meta.env.PUBLIC_BASE_URL || "/portfolio/"; // Default to /portfolio/ if not set
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://maulasyakur.github.io",
-  base: import.meta.env.BASE_URL, // Must match your GitHub repository name exactly
+  base: baseUrl, // Must match your GitHub repository name exactly
   output: "static", // GitHub Pages only supports static sites
 });
