8ba3fdce3d86e5ce885bc72347494520127bd28d
maulasyakur <maulaahmad18@gmail.com>
2026-07-31 10:33:16 +0300

    fix navigation

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://maulasyakur.github.io",
-  base: "/portfolio", // Must match your GitHub repository name exactly
+  base: import.meta.env.BASE_URL, // Must match your GitHub repository name exactly
   output: "static", // GitHub Pages only supports static sites
 });
